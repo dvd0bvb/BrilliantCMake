73969788b274d5e63f281b1cb47f7758438d6e43
David Brill <6david6brill6@gmail.com>
2024-07-26 12:39:34 -0400

    possible fix for gtest bash file

--- .github/actions/install-gtest/install-gtest.sh
+++ .github/actions/install-gtest/install-gtest.sh
@@ -7,7 +7,8 @@ git clone https://github.com/google/googletest.git --branch $BRANCH --depth 1
 pushd googletest
 mkdir build
 pushd build
-if [-z "$3"] then
+if [-z "$3"] 
+then
     cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE -G "Unix Makefiles"
 else
     cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE -G "Unix Makefiles" -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_FLAGS=$3 -DCMAKE_CXX_FLAGS=$3
