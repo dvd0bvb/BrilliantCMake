0cc210a5cdd52b104cfdffea4cd90e7c2d83a94d
David Brill <6david6brill6@gmail.com>
2024-07-26 12:24:41 -0400

    more clang and msan things

--- .github/actions/install-gtest/install-gtest.sh
+++ .github/actions/install-gtest/install-gtest.sh
@@ -7,6 +7,10 @@ git clone https://github.com/google/googletest.git --branch $BRANCH --depth 1
 pushd googletest
 mkdir build
 pushd build
-cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE -G "Unix Makefiles"
+if [-z "$3"] then
+    cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE -G "Unix Makefiles"
+else
+    cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE -G "Unix Makefiles" -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_FLAGS=$3 -DCMAKE_CXX_FLAGS=$3
+fi
 make 
 sudo make install
