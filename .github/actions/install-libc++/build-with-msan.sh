1f1831ebb287f09160033064a1c4b7a13aceef61
David Brill <6david6brill6@gmail.com>
2024-07-26 12:40:47 -0400

    Fix for msan action

--- .github/actions/install-libc++/build-with-msan.sh
+++ .github/actions/install-libc++/build-with-msan.sh
@@ -6,7 +6,7 @@ git clone --depth=1 https://github.com/llvm/llvm-project
 cd llvm-project
 mkdir build; cd build
 # configure cmake
-cmake -G "Unix Makefile" ../llvm \
+cmake -G "Unix Makefiles" ../llvm \
 	-DCMAKE_BUILD_TYPE=Release \
 	-DLLVM_ENABLE_PROJECTS="libcxx;libcxxabi" \
 	-DCMAKE_C_COMPILER=clang \
