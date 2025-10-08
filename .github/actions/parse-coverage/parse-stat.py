a4323fcc8193856615f562d2cadd40988cfc7967
David Brill <6david6brill6@gmail.com>
2025-10-08 19:38:38 -0400

    #11 Update coverage workflow to support dynamic badge

--- .github/actions/parse-coverage/parse-stat.py
+++
@@ -1,9 +0,0 @@
-from argparse import ArgumentParser
-import json
-
-parser = ArgumentParser()
-parser.add_argument('json')
-parser.add_argument('field')
-args = parser.parse_args()
-data = json.loads(args.json)
-print(data[args.field])
